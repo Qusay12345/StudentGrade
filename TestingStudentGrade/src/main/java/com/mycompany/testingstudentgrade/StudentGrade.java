582c780522975a0f954b333e274dc2f10cc37197
Qusay12345 <165953615+Qusay12345@users.noreply.github.com>
2024-04-05 14:32:00 +0300

    Update StudentGrade.java

--- TestingStudentGrade/src/main/java/com/mycompany/testingstudentgrade/StudentGrade.java
+++ TestingStudentGrade/src/main/java/com/mycompany/testingstudentgrade/StudentGrade.java
@@ -3,12 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.testingstudentgrade;
+import java.util.Scanner;
 
 /**
  *
  * @author Windows.10
  */
 public class StudentGrade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the student mark:");
+            System.out.println("Student grade is " +
+                    getGrade(scan.nextInt()));
+
+    } 
     /**
      * Calculates grade as follows:
      * A (90 - 100), B (80 - 89), C(<80 && >=70), D(<70 && >=60) F ( < 60)
